bae1fe268de55fa15302f92fd6f2a5b140973511
Ange P <angegolding@gmail.com>
2024-09-24 06:02:59 +0100

    IoC Container complete

--- exercises/software-design/src/main/java/com/cbfacademy/App.java
+++ exercises/software-design/src/main/java/com/cbfacademy/App.java
@@ -1,14 +1,27 @@
 package com.cbfacademy;
 
 public class App {
+
+    private static SimpleContainer initialiseContainer() {
+        SimpleContainer container = new SimpleContainer();
+
+        // Register mappings for any required interfaces with their concrete implementations
+        container.register(Game.class, DiceGame.class);
+        container.register(Player.class, DicePlayer.class);
+        return container;
+    }
     public static void main(String[] args) {
-        Game game = GameFactory.create();
+        SimpleContainer container = initialiseContainer();
+        Game game = container.get(Game.class);
         String winner = game.play();
 
         if (winner == null) {
             System.out.println("It's a draw!");
         } else {
             System.out.println(winner + " wins!");
-        }
+        }     
     }
+
+
+
 }
