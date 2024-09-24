a1b7a72d976f847a25ed988397bc2b2f2cc1a13d
Ange P <angegolding@gmail.com>
2024-09-24 05:13:21 +0100

    Inversion of Control completed

--- exercises/software-design/src/main/java/com/cbfacademy/DiceGame.java
+++ exercises/software-design/src/main/java/com/cbfacademy/DiceGame.java
@@ -1,13 +1,13 @@
 package com.cbfacademy;
 
 public class DiceGame {
-    private DicePlayer player1;
-    private DicePlayer player2;
+    private DicePlayer player1 = PlayerFactory.create();
+    private DicePlayer player2 = PlayerFactory.create();
     private int targetScore = 30;
 
     public DiceGame() {
-        player1 = new DicePlayer();
-        player2 = new DicePlayer();
+        //player1 = new DicePlayer();
+        //player2 = new DicePlayer();
         player1.setName("Player 1");
         player2.setName("Player 2");
     }
